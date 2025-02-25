7d6c611a9a8f05364a42141c4bf24cbfd13943c5
ArjunDev17 <arjun.devb25@gmail.com>
2025-02-25 23:27:06 +0530

    find Max avg

---
+++ 13th-day/findMaxAverage.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func findMaxAvg(arr []int, k int) float64 {
+	currentSum := 0
+	for i := 0; i < k; i++ {
+		currentSum += arr[i]
+	}
+	maxSum := currentSum
+	for i := k; i < len(arr); i++ {
+		currentSum = arr[i] - arr[i-k]
+
+		// if maxSum < currentSum {
+		// 	maxSum = currentSum  maximum avg is : 12.25
+		// }
+		if currentSum > maxSum {
+			maxSum = currentSum
+		}
+	}
+	return float64(maxSum) / float64(k)
+}
+func main() {
+	arr := []int{1, 12, -5, -6, 50, 3}
+	k := 4
+	avgIs := findMaxAvg(arr, k)
+	fmt.Println("maximum avg is :", avgIs)
+}
